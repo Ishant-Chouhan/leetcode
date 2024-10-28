9aa38bb958b0f9eab607649fcdbf8a35e9f5b8ce
Ishant_Chouhan <145678685+Ishant-Chouhan@users.noreply.github.com>
2024-10-28 22:00:22 +0530

    Time: 0 ms (100.00%), Space: 11.7 MB (32.54%) - LeetHub

---
+++ 0168-excel-sheet-column-title/0168-excel-sheet-column-title.py
@@ -0,0 +1,12 @@
+class Solution:
+    def convertToTitle(self, columnNumber):
+        ans = ""
+        while columnNumber > 0:
+            columnNumber -= 1
+            # Get the last character and append it at the end of string.
+            ans += chr(columnNumber % 26 + ord("A"))
+            columnNumber //= 26
+
+        # Reverse it, as we appended characters in reverse order.
+        return ans[::-1]
+        
