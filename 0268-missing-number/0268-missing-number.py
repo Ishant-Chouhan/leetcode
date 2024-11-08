0eaa845a5cff187b10455206bc15f19cc7cecab8
Ishant_Chouhan <145678685+Ishant-Chouhan@users.noreply.github.com>
2024-11-08 10:44:12 +0530

    Time: 0 ms (100.00%), Space: 12.6 MB (82.62%) - LeetHub

--- 0268-missing-number/0268-missing-number.py
+++ 0268-missing-number/0268-missing-number.py
@@ -4,10 +4,5 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        #running loop upto 1+length of array since it is exclusive value
-        for i in range(len(nums)+1):
-            #checking weather i is present in array or not
-            if i not in nums:
-                
-                return i 
+        return sum(range(0,len(nums)+1))-sum(nums)
         
