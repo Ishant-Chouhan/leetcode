23198efab40f9cede3435907d99a1b43339a716f
Ishant_Chouhan <145678685+Ishant-Chouhan@users.noreply.github.com>
2024-11-30 12:49:24 +0530

    Time: 890 ms (10.86%), Space: 13.1 MB (5.44%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def canJump(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        target = len(nums) - 1
+        for i in range(len(nums) - 2, -1,-1):
+            print(nums[i])
+            if target <= i + nums[i]:
+                target = i
+        return target == 0
