1c2b0eeef8c2c1d062f293cfeb584ca150e0061e
Ishant_Chouhan <145678685+Ishant-Chouhan@users.noreply.github.com>
2025-01-17 08:42:10 +0530

    Time: 0 ms, Memory: 12.5 MB - LeetHub

---
+++ 3514-1743-263-ugly-number/3514-1743-263-ugly-number.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def isUgly(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+        for p in 2, 3, 5:
+            while n % p == 0 < n:
+                n /= p
+        return n == 1
+
+                    
+        
